0693f9a2c9ed66d9943ab54f8f694083c71047b8
Douglas Fabio <douglasfabio.ifsp@gmail.com>
2023-08-16 12:57:21 -0300

    order by

--- ApiTravelogue/ApiTravelogue/Services/ViagemServices.cs
+++ ApiTravelogue/ApiTravelogue/Services/ViagemServices.cs
@@ -17,7 +17,7 @@ namespace ApiTravelogue.Services
                 (produtoServices.Value.ViagemCollectionName);
         }
 
-        public async Task<List<Viagem>> GetAsync() => await _viagemCollection.Find(v => true).ToListAsync();
+        public async Task<List<Viagem>> GetAsync() => await _viagemCollection.Find(v => true).Sort(Builders<Viagem>.Sort.Descending(v => v.DateTravel)).ToListAsync();
         public async Task<Viagem> GetAsync(string id) => await _viagemCollection.Find(v => v.Id == id).FirstOrDefaultAsync();
 
         public async Task CreateAsync (Viagem viagem) => await _viagemCollection.InsertOneAsync(viagem);
